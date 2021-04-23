4fd66a383015421afe12f98d0f5fad2306f6001b
Efosa Ighodaro <efosaighodaroei@gmail.com>
2021-04-22 22:12:14 -0400

    Frontend changes

--- src/customerserver.ts
+++ src/customerserver.ts
@@ -30,6 +30,7 @@ app.use(express.urlencoded({
 
 app.use(express.static(__dirname));
 
+
 // list out all the customers and their info
 app.get('/api/all/',(req,res) =>{
     db.all("SELECT rowid AS id, name FROM customers", function(err,rows){
@@ -110,8 +111,9 @@ app.delete('/api/:name', (req,res)=>{
     })
 })
 app.get('*', function(req,res){
-  res.sendFile(path.join(__dirname+ '/index.html'))
-})
+    res.sendFile(path.join(__dirname+ '/index.html'))
+});
+
 app.listen(port, () =>{
    return console.log('listening on port 3000')
 });
